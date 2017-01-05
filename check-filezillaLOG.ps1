4db1091a207b32a3d7a2a71a4cd529f3cc9068ad
Kjetil Grun <kjetil.grun@gmail.com>
2017-01-05 15:51:31 +0100

    Branch is working
    Script is working and posting to slack.

--- check-filezillaLOG.ps1
+++ check-filezillaLOG.ps1
@@ -14,7 +14,7 @@
   Version:        2.0
   Author:         Kjetil Grun
   Creation Date:  27.06.2016
-  Purpose/Change: Just a working script
+  Purpose/Change: Divided the different jobs into function and sent the new files noticed upload in one message.
   
 .EXAMPLE
   <Example goes here. Repeat this attribute for more than one example>
@@ -25,21 +25,14 @@
 #Log location
 $fzdir = 'C:\Program Files (x86)\FileZilla Server\Logs\'
 $line = 0
-$global:lastfile = $null
-$global:line = $null
+$lastfile = ''
+
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
-
-function run-forever {
-	while($true) {
-		check-ifnewfile
-		}
-}
-
 function check-ifnewfile {
 	# Finding last changed logfile
 	$fzlog = Get-ChildItem $fzdir | Where-Object { ! $_.PSIsContainer } | Sort-Object lastwriteTime | Select-Object -last 1
 	write-host "checking if the file is new"
-	if($lastfile -like $null -or $fzdir+$fzlog -notlike $lastfile) {
+	if($lastfile -like '' -or $fzdir+$fzlog -notlike $lastfile) {
 			Write-Host 'New file. Reset line counter'
 			$line = 0
 	}
@@ -59,42 +52,37 @@ function find-logfilesize {
 function check-lineschanged {
 	# Checking if number of lines has changed
 	if($currlines.Lines -gt $line) {
-		# If number of file is more than last time check the newest lines
+		# If number of lines is more than last time - check the newest lines
 		(Get-Content (Get-Item $fzdir$fzlog))[$line .. $currlines.Lines]| foreach {
 			$word = $_.split(' ')
-			# For loop through the line as it seems different powershell versions has different ways of counting the array. It should not be less than 7 words into the sentence.
+			# For loop through the line as it seems different powershell versions has different ways of counting the list. It should not be less than 7 words into the sentence.
 			for($i=0; $i -le 6; $i++) {
-			  # Finding the word STOR which is the status code for stored file in FileZilla and adding it to an array 
+			  # Finding the word STOR which is the status code for stored file in FileZilla and adding it to an list 
 			  if($word[$i] -like 'STOR') {
-				$msgarray +=(,($word[$i+1],$word[$i-1],$word[$i-2]))	
+				#$msglist +=(,($word[$i+1]," is uploaded by ",$word[$i-1]," with IP: ",$word[$i-2]))	
+				$msglist +=($word[$i+1]+" is uploaded by "+$word[$i-1]+" with IP: "+$word[$i-2]+"`n")	
 			  }
 			}
 		}
 	}
 		# Setting the value of the line numbers and filename for the next check.
-		set-globallines $currlines.lines [REF]$global:line
-		write-host $line "er antall linjer"
-		#Set-Variable -Name $line -Value $currlines.Lines
-		Set-variable -Name $lastfile -Value $fzdir+$fzlog
+		$line = $currlines.Lines
+		$lastfile = $fzdir+$fzlog
 		send-message
 }
 
-function set-globallines ($a, [REF]$b) {
-	$b.value = $currlines.Lines
-}
-
-
 function send-message {
 
-	#$postSlackMessage = @{token='toke-here';channel='#compile-bot';text="$file uploaded by $ip to $site";username='via API'}
-	#Invoke-RestMethod -Uri https://slack.com/api/chat.postMessage -Body $postSlackMessage
-	for($j=0; $j -le $msgarray.GetUpperBound(0); $j++) {
-		write-host $msgarray[$j][0] "is uploaded by" $msgarray[$j][1] "with IP:" $msgarray[$j][2]
+	
+	if($msglist -notlike $null){
+		$postSlackMessage = @{token="notoken4ugetyerown";channel='compile-bot';as_user="false";icon_url="icon url";text=$msglist;username="jian-yang"}
+		Invoke-RestMethod -Uri https://slack.com/api/chat.postMessage -Body $postSlackMessage
+		# Clearing the list
+		clear-variable -Name "msglist"
 	}
-	$msgarray.Clear()
 	# Sleeping for X seconds so not to check all the time
 	Start-Sleep -s 10
-
+	check-ifnewfile 
 }
 
-run-forever
+check-ifnewfile
